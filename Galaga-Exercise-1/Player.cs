0061a407d9a9d8bcbce124bf74353e2841821739
Nigoleha <niels.goettge.lerche@live.dk>
2019-02-27 10:26:50 +0100

    Method til player

--- Galaga-Exercise-1/Player.cs
+++ Galaga-Exercise-1/Player.cs
@@ -1,5 +1,6 @@
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
+using DIKUArcade.Math;
 
 namespace Galaga_Exercise_1 {
     public class Player : Entity {
@@ -7,6 +8,13 @@ namespace Galaga_Exercise_1 {
         public Player(Game game, DynamicShape shape, IBaseImage image)
             : base(shape, image) {
             this.game = game;
+        }
+
+        public void Direction(Vec2F) {
+            //add code
+        }
+        public void Move() {
+            //add code
         }
     }
 }
