81a87c632a3588faf80ddd918c5751dad9e1c93e
carolinesahl <carolinesahlholt@gmail.com>
2019-02-27 11:10:28 +0100

    PlayerShots class + list playershots game

---
+++ Galaga-Exercise-1/PlayerShot.cs
@@ -0,0 +1,14 @@
+using System.IO.Pipes;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+
+namespace Galaga_Exercise_1 {
+    public class PlayerShot: Entity {
+        private Game game;
+
+        public Player(Game game, DynamicShape shape, IBaseImage image)
+            : base(shape, image) {
+            this.game = game;
+            
+        }
+    }
